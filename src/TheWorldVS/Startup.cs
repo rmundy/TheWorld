6d8687da6d2e9027d7b563b64d968168a80459f9
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-09 15:40:58 -0500

    Finished HTML and CSS Basics

--- src/TheWorldVS/Startup.cs
+++ src/TheWorldVS/Startup.cs
@@ -23,10 +23,12 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync($"Hello World!: {context.Request.Path}");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello World!: {context.Request.Path}");
+            //});
         }
 
         // Entry point for the application.
