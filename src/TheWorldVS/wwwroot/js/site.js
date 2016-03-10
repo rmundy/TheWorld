45058b5bfbfae3e2a5c0dc15cb568bcac4033b66
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-09 22:41:25 -0500

    Added Javascript

--- src/TheWorldVS/wwwroot/js/site.js
+++ src/TheWorldVS/wwwroot/js/site.js
@@ -1,18 +1,30 @@
 ﻿// site.js
 (function () {
+    //var main = $("#main");
+    
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+    
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "#eee");
+    //});
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
-    });
+    //var menuItems = $("#ul.menu li a");
+    //menuItems.click(function () {
+    //    var me = $(this);
+    //    me.text = "Hi";
+    //    alert(me.text());
+    //});
 
-    main.on("mouseleave", function () {
-        main.style = "";
+    var sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").click(function () {
+        sidebarAndWrapper.toggleClass("hide-sidebar");
+        if (sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     });
-
-    var menuItems = $("#ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert("Hello");
-    });
+    
 }());
