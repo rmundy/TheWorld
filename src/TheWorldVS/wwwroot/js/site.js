eb6a7f48ae3486fe4a7d10de765722891a20dc5a
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-09 21:47:59 -0500

    Added jquery and underscore

---
+++ src/TheWorldVS/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// site.js
+(function () {
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuItems = $("#ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert("Hello");
+    });
+}());
