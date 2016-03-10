2e676df33be2ae55c45b85b5f935eabd522ca23c
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-10 17:27:39 -0500

    Added tag helpers

--- src/TheWorldVS/Controllers/AppController.cs
+++ src/TheWorldVS/Controllers/AppController.cs
@@ -9,5 +9,15 @@ namespace TheWorldVS.Controllers.Web
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
