09f70abadb6b99c7badc3eb9abb756add95b8fe8
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-21 18:25:03 -0400

    Added Jquery validation

---
+++ src/TheWorldVS/ViewModels/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TheWorldVS.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Serializable]
+    public sealed class ContactViewModel
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public String Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
+        public String Message { get; set; }
+    }
+}
