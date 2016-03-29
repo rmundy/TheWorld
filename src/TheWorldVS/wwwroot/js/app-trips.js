6a557742c3f334eff0cc464a29ac37f0ed0e0fc9
Bobby Mundy <robert.l.mundy@gmail.com>
2016-03-29 12:03:31 -0400

    Added ngRoute

---
+++ src/TheWorldVS/wwwroot/js/app-trips.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app-trips', [
+        // Angular modules 
+        'ngRoute'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+})();
---
+++ src/TheWorldVS/wwwroot/js/tripsController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app-trips')
+        .controller('tripsController', tripsController);
+
+    tripsController.$inject = ['$scope']; 
+
+    function tripsController($scope) {
+        $scope.title = 'tripsController';
+        $scope.name = 'Bobby';
+        $scope.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+
+        activate();
+
+        function activate() { }
+    }
+})();
+
